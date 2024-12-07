aff59bcea4cc670473a21f6e76b48481121b337b
Curtis Clement <curtis.clement@wonderkind.com>
2024-12-07 22:09:24 +0100

    add add_place widget

--- lib/widgets/places_list.dart
+++ lib/widgets/places_list.dart
@@ -9,7 +9,14 @@ class PlacesList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     if (places.isEmpty) {
-      return const Center(child: Text('No places found. Start adding some!'));
+      return Center(
+        child: Text(
+          'No places found. Start adding some!',
+          style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+        ),
+      );
     }
 
     return ListView.builder(
